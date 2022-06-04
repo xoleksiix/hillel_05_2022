8f4d3f5d772ff9948d33967d6851ca7697da0a9b
oleksii <19oleksii96@gmail.com>
2022-06-04 09:46:50 +0300

    add

---
+++ lesson_3_code_quality/classwork/examples.py
@@ -0,0 +1,22 @@
+# yapf
+def foosdasdasdkasdajdsdasdadasdas(
+    number_1: int,
+    number_2: str,
+    number_3: str,
+    number_4: str,
+) -> None:
+    pass
+
+
+# black
+def foosdasdasdkasdajdsdasdadasdas2(
+    number_1: int,
+    number_2: str,
+    number_3: int,
+    number_4: str,
+) -> None:
+    pass
+
+
+def bar(arg_1):
+    return 4
