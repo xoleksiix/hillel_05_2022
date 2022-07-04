a4a884edfd8f2206a7ffa33d2df2afa77e4273a6
oleksii <19oleksii96@gmail.com>
2022-07-04 09:31:08 +0300

    add 11

---
+++ lesson_11_async_threads/homework/async.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+import asyncio
+
+print(datetime.now())
+
+
+async def get_primes_amount(num: int) -> int:
+    result = 1
+    for i in range(2, num+1):
+        result += 1
+        for j in range(2, i):
+            if i % j == 0:
+                result -= 1
+                break
+        await asyncio.sleep(0)
+    print(result)
+    return result
+
+
+numbers = [40000, 400, 1000000, 700]
+
+tasks = [get_primes_amount(i, ) for i in numbers]
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.gather(*tasks))
+loop.close()
+
+print(datetime.now())
+
+# for i in numbers:
+#     print(get_primes_amount(i))
+
+# NOTE: Щож. Ця реалізація займає досить багато часу...
+#       Було б чудово, якби люди, які передають менші числа отримували результатми швидше ніж ті, хто передають великі значення
+
+# TODO: Закінчіть цю функцію
+# TODO: Застосуйте асинхронне програмування
---
+++ lesson_11_async_threads/homework/threads.py
@@ -0,0 +1,41 @@
+from threading import Thread
+from typing import Union
+import random
+
+
+random_int = []
+
+
+def random_numbers():
+    for i in range(10_000):
+        random_int.append(random.randint(0, 10000))
+
+
+def sum(x: list) -> int:
+    count = 0
+    for i in x:
+        count += i
+    print(count)
+
+
+def avarage(x: list) -> Union[int, float]:
+    count = 0
+    for i in x:
+        count += i
+    print(count / len(x))
+
+
+def main():
+    t1 = Thread(target=random_numbers)
+    t1.start()
+    t1.join()
+    t2 = Thread(target=sum, args=(random_int,))
+    t3 = Thread(target=avarage, args=(random_int,))
+    t2.start()
+    t3.start()
+    t2.join()
+    t3.join()
+
+
+if __name__ == "__main__":
+    main()
